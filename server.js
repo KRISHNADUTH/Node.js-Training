45eedf405f3cb4a5dfce32a4a3596e81c9e94218
Krishnaduth <krishnaduth777@gmail.com>
2023-07-02 12:07:05 +0530

    2nd Changes

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const res = require('express/lib/response');
-const app = express()
-
-const port = 3000
+const app = express();
+const port = 8001
+const mongoUrl = 'mongodb://Khabib:Khabib123@ac-d17bovc-shard-00-00.cdefmn6.mongodb.net:27017,ac-d17bovc-shard-00-01.cdefmn6.mongodb.net:27017,ac-d17bovc-shard-00-02.cdefmn6.mongodb.net:27017/?ssl=true&replicaSet=atlas-zw0rcy-shard-0&authSource=admin&retryWrites=true&w=majority';
+mongoose.connect(mongoUrl).then(() => console.log("Connected"));
 
 app.get('/', (req, res) => {
     res.send("Hello world")
